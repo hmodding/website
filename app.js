57027d58ef93364aec43bc2de68367682ccd66b4
traxam <tr808axm@gmail.com>
2018-12-20 18:46:11 +0100

    Added a background image and improved the error page.

--- app.js
+++ app.js
@@ -36,6 +36,9 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+
+  // prints the error
+  console.log(err);
 });
 
 module.exports = app;
