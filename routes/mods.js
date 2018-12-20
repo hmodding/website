cae9b2987358c18841e60b0c66ec2a9da7703a74
traxam <tr808axm@gmail.com>
2018-12-21 00:13:07 +0100

    Added basic mod page. (#3)

--- routes/mods.js
+++ routes/mods.js
@@ -5,5 +5,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('mods');
 });
+router.get('/:id', function (req, res, next) {
+  var mod = {
+    id: req.id,
+    title: 'Mod Title',
+    description: 'This is a mod about whatever and something else, that has a long description.',
+    readme: 'asdf',
+    category: 'Utility',
+    version: '1.0',
+    author: 'traxam'
+  };
+  res.render('mod', {mod: mod});
+});
 
 module.exports = router;
