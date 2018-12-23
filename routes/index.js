b662aa8c19d858c830b7f41fac6c8a06b2d80622
traxam <tr808axm@gmail.com>
2018-12-23 21:50:03 +0100

    Added sign-in dialogue. (#8)

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,36 @@ var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+    res.render('index');
 });
 router.get('/forum', function(req, res, next) {
-  res.redirect('https://forum.raftmodding.com/');
+    res.redirect('https://forum.raftmodding.com/');
 });
 router.get('/discord', function(req, res, next) {
-  res.redirect('https://discord.gg/raft');
+    res.redirect('https://discord.gg/raft');
 });
 router.get('/docs', function(req, res, next) {
-  res.redirect('https://docs.raftmodding.com/');
+    res.redirect('https://docs.raftmodding.com/');
 });
 router.get('/download', function(req, res, next) {
-  res.render('download', {versions: JSON.parse(fs.readFileSync('versions.json'))});
+    res.render('download', {versions: JSON.parse(fs.readFileSync('versions.json'))});
+});
+router.get('/signin', function (req, res, next) {
+    res.render('signin');
+});
+router.post('/signin', function(req, res, next) {
+    // check and redirect
+    var loginCorrect = false;
+    if (loginCorrect)
+        res.redirect(req.param);
+    else
+        res.render('signin');
+});
+router.get('/signup', function (req, res, next) {
+    res.render('signup');
+});
+router.get('/forgotpassword', function (req, res, next) {
+    res.render('forgotpassword');
 });
 
 module.exports = router;
