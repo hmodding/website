92237d678efce9247fb03b21a0343f83825dc67c
traxam <tr808axm@gmail.com>
2019-02-10 15:50:20 +0100

    Added form for adding mods.

--- routes/mods.js
+++ routes/mods.js
@@ -7,26 +7,29 @@ var markdownConverter = new showdown.Converter({extensions: [xssFilter]});
 var mods = JSON.parse(fs.readFileSync('mods.json'));
 
 function getModById(id) {
-  for (var i = 0; i < mods.length; i++) {
-    if (mods[i].id.toLowerCase() === id.toLowerCase())
-      return mods[i];
-  }
-  return null;
+    for (var i = 0; i < mods.length; i++) {
+        if (mods[i].id.toLowerCase() === id.toLowerCase())
+            return mods[i];
+    }
+    return null;
 }
 
 /* GET mods listing */
 router.get('/', function(req, res, next) {
-  res.render('mods', {title: 'Mods', mods: mods});
+    res.render('mods', {title: 'Mods', mods: mods});
+});
+router.get('/add', (req, res) => {
+    res.render('addmod', {title: 'Add a mod'});
 });
 router.get('/:id', function (req, res, next) {
-  var mod = getModById(req.params.id);
-  if (mod === null)
-    res.render('error', {error: {status: 404}});
-  else {
-    // render markdown readme
-    mod.readmeMarkdown = markdownConverter.makeHtml(mod.readme.replace(/</g, '&lt;').replace(/>/g, '&gt;'));
-    res.render('mod', {title: mod.title, mod: mod});
-  }
+    var mod = getModById(req.params.id);
+    if (mod === null)
+        res.render('error', {error: {status: 404}});
+    else {
+        // render markdown readme
+        mod.readmeMarkdown = markdownConverter.makeHtml(mod.readme.replace(/</g, '&lt;').replace(/>/g, '&gt;'));
+        res.render('mod', {title: mod.title, mod: mod});
+    }
 });
 
 module.exports = router;
