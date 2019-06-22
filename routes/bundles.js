494a181f95562bb627b170832b7235ee6d9def4b
Polyfox <tr808axm@gmail.com>
2019-06-22 10:11:00 +0200

    Added mod bundle adding backend. (#79)

--- routes/bundles.js
+++ routes/bundles.js
@@ -5,6 +5,7 @@ module.exports = (logger, db, fileScanner) => {
   var router = express.Router();
   var convertMarkdown = require('../markdownConverter');
   var querystring = require('querystring');
+  var urlRegexp = !/(http[s]?:\/\/)?[^\s(["<,>]*\.[^\s[",><]*/;
 
   /**
    * Finds the bundle from the bundleId provided in the URL path.
@@ -54,10 +55,50 @@ module.exports = (logger, db, fileScanner) => {
     }
   };
 
-  router.get('/add', requireLogin, (req, res, next) => {
-    res.locals.formContents = req.query;
-    res.render('bundle/add', {title: 'Create mod bundle'});
-  });
+  router.route('/add')
+    .get(requireLogin, (req, res, next) => {
+      res.render('bundle/add', {title: 'Create mod bundle'});
+    })
+    .post(requireLogin, (req, res, next) => {
+      res.locals.formContents = req.body;
+      res.locals.title = 'Add mod bundle';
+      var modBundle = {
+        title: req.body.title,
+        description: req.body.description,
+        readme: req.body.readme,
+        maintainerId: req.session.user.id,
+        bannerImageUrl: req.body.bannerImageUrl,
+      };
+      if (!modBundle.title ||
+          !modBundle.description ||
+          !modBundle.readme) {
+        res.render('bundle/add', {error: 'All fields of this form need to be ' +
+          'filled to submit a mod bundle.'});
+      } else if (modBundle.title.length > 100) {
+        res.render('bundle/add', {error: 'The title can not be longer than ' +
+          '100 characters!'});
+      } else if (modBundle.description.length > 255) {
+        res.render('bundle/add', {error: 'The description can not be longer ' +
+          'than 255 characters! Please use the readme section for longer ' +
+          'explanations.'});
+      } else if (modBundle.bannerImageUrl &&
+          !urlRegexp.test(modBundle.bannerImageUrl)) {
+        res.render('bundle/add', {error: 'The banner image URL must be a ' +
+          'valid URL or can be left emtpy.'});
+      } else {
+        db.ModBundle.create(modBundle)
+          .then(bundleInst => {
+            res.redirect(`/bundle/${bundleInst.id}`);
+            logger.info(`Mod bundle ${bundleInst.title} (${bundleInst.id}) ` +
+              `was created by user ${req.session.user.username}.`);
+          })
+          .catch(err => {
+            res.render('bundle/add', {error: 'An unexpected error occurred. ' +
+              'Please try again.'});
+            logger.error('Unexpected DB error in mod bundle creation: ', err);
+          });
+      }
+    });
 
   router.get('/:bundleId', bundleMiddleware, checkOwnership,
     (req, res, next) => {
