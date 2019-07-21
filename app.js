8b32d3dae4600113d9f50cb8257b1b8c19a26769
traxam <tr808axm@gmail.com>
2019-07-21 17:27:47 +0200

    Added switch for old vs. new branding.

--- app.js
+++ app.js
@@ -57,6 +57,7 @@ app.use((req, res, next) => {
   res.locals.googleAnalyticsId = credentials.googleAnalyticsId;
   res.locals.enableBundlesSection = credentials.enableBundlesSection;
   res.locals.enablePluginsSection = credentials.enablePluginsSection;
+  res.locals.newBranding = credentials.newBranding;
   next();
 });
 
