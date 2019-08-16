326562240a90e76683774a539d3a314d41fbd277
Polyfox <tr808axm@gmail.com>
2019-08-16 08:43:28 +0200

    Implemented server-version-adding frontend. (#98)

--- routes/server.js
+++ routes/server.js
@@ -3,7 +3,14 @@ module.exports = (logger, db, fileScanner) => {
   const router = require('express').Router();
   const createError = require('http-errors');
   const convertMarkdown = require('../markdownConverter');
+  const multer = require('multer');
+  const upload = multer({storage: multer.memoryStorage()});
 
+  /**
+   * Middleware function to find a server version based on the url path and
+   * save the corresponding database instance to req.serverVersion and
+   * res.locals.serverVersion. 
+   */
   function findServerVersion(req, res, next) {
     var version = req.params.serverVersion;
     db.ServerVersion.findOne({where: {version}})
@@ -17,6 +24,18 @@ module.exports = (logger, db, fileScanner) => {
       .catch(next);
   }
 
+  /**
+   * Middleware function that nexts to a 403 error if the logged in user is not
+   * an admin.
+   */
+  function requireAdmin(req, res, next) {
+    if (res.locals.userIsAdmin) {
+      next();
+    } else {
+      res.status(403).render('error', {error: {status: 403}});
+    }
+  }
+
   router.get('/', (req, res, next) => {
     db.ServerVersion.findAll({order: [['timestamp', 'DESC']]})
       .then(versions => {
@@ -24,6 +43,14 @@ module.exports = (logger, db, fileScanner) => {
       })
       .catch(next);
   });
+
+  router.route('/add')
+    .get(requireAdmin, (req, res, next) => {
+      res.render('server/add', {
+        title: 'Add a server version',
+        formContents: {},
+      });
+    });
 
   router.get('/:serverVersion', findServerVersion, (req, res, next) => {
     req.serverVersion.changelogHTML =
