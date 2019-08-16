734196f7f3880d845a1361e23759dbaf21c31d0e
Polyfox <tr808axm@gmail.com>
2019-08-16 08:11:10 +0200

    Added server display page. (#94)

--- routes/server.js
+++ routes/server.js
@@ -1,6 +1,21 @@
 'use strict';
 module.exports = (logger, db, fileScanner) => {
-  var router = require('express').Router();
+  const router = require('express').Router();
+  const createError = require('http-errors');
+  const convertMarkdown = require('../markdownConverter');
+
+  function findServerVersion(req, res, next) {
+    var version = req.params.serverVersion;
+    db.ServerVersion.findOne({where: {version}})
+      .then(serverVersion => {
+        if (!serverVersion) return Promise.reject(createError(404));
+        else {
+          req.serverVersion = res.locals.serverVersion = serverVersion;
+          next();
+        }
+      })
+      .catch(next);
+  }
 
   router.get('/', (req, res, next) => {
     db.ServerVersion.findAll({order: [['timestamp', 'DESC']]})
@@ -8,6 +23,14 @@ module.exports = (logger, db, fileScanner) => {
         res.render('server/list', {title: 'Dedicated server', versions});
       })
       .catch(next);
+  });
+
+  router.get('/:serverVersion', findServerVersion, (req, res, next) => {
+    req.serverVersion.changelogHTML =
+      convertMarkdown(req.serverVersion.changelog);
+    res.render('server/server', {
+      title: 'Dedicated server ' + req.serverVersion.version
+    });
   });
 
   return router;
