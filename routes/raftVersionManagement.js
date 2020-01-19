69145ca72309fe4b9c98e75820b92387db080fa2
traxam <tr808axm@gmail.com>
2020-01-19 17:03:08 +0100

    feat(raft-versions): added editing form (#201)

--- routes/raftVersionManagement.js
+++ routes/raftVersionManagement.js
@@ -26,5 +26,64 @@ module.exports = (logger, db, fileScanner) => {
       .catch(next);
   });
 
+  /**
+   * Page for editing raft versions.
+   */
+  router.route('/:id')
+    .get((req, res, next) => {
+      if (!parseInt(req.params.id, 10)) next(createError(404));
+      else {
+        db.RaftVersion.findOne({where: {id: req.params.id}})
+          .then(raftVersion => {
+            if (!raftVersion) next(createError(404));
+            else {
+              res.render('raft-version-management/edit', {
+                raftVersion,
+                formContents: raftVersion,
+              });
+            }
+          })
+          .catch(next);
+      }
+    })
+    .post((req, res, next) => {
+      if (!parseInt(req.params.id, 10)) next(createError(404));
+      else {
+        db.RaftVersion.findOne({where: {id: req.params.id}})
+          .then(raftVersion => {
+            res.locals.formContents = req.body;
+            res.locals.raftVersion = raftVersion;
+            if (!raftVersion) next(createError(404));
+            else {
+              let respondError =
+              error => res.render('raft-version-management/edit', {error});
+              let update = {
+                buildId: req.body.buildId,
+                title: req.body.title,
+                releasedAt: req.body.releasedAt,
+              };
+
+              if (!update.buildId || !update.title || !update.releasedAt) {
+                respondError('All fields of this forms are required!');
+              } else {
+                raftVersion.update(update)
+                  .then(newValues => {
+                    res.render('raft-version-management/edit', {
+                      success: 'Updated successfully!',
+                      raftVersion: newValues,
+                      formContents: newValues,
+                    });
+                  })
+                  .catch(err => {
+                    respondError(err);
+                    logger.error(err);
+                  });
+              }
+            }
+          })
+          .catch(next);
+      }
+    });
+
   return router;
 };
