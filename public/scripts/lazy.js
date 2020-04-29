2daf9448afc8c51a1480b6e49238b00f69ae7d39
traxam <tr808axm@gmail.com>
2020-04-29 16:12:49 +0200

    Format lazy.js (#214)

--- public/scripts/lazy.js
+++ public/scripts/lazy.js
@@ -1,27 +1,30 @@
+/* eslint-disable no-undef */
+'use strict';
+
 /*
  * Lazy image loading.
  */
-document.addEventListener("DOMContentLoaded", function() {
-    var lazyImages = [].slice.call(document.querySelectorAll("img.lazy-image"));
+document.addEventListener('DOMContentLoaded', function() {
+  var lazyImages = [].slice.call(document.querySelectorAll('img.lazy-image'));
 
-    if ("IntersectionObserver" in window) {
-        var lazyImageObserver = new IntersectionObserver(function(entries, observer) {
-            entries.forEach(function(entry) {
-                if (entry.isIntersecting) {
-                    var lazyImage = entry.target;
-                    if (lazyImage.dataset.src) {
-                        lazyImage.src = lazyImage.dataset.src;
-                        lazyImage.classList.remove("lazy-image");
-                        lazyImageObserver.unobserve(lazyImage);
-                    }
-                }
-            });
-        });
+  if ('IntersectionObserver' in window) {
+    var lazyImageObserver = new IntersectionObserver(function(entries, obs) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          var lazyImage = entry.target;
+          if (lazyImage.dataset.src) {
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.classList.remove('lazy-image');
+            lazyImageObserver.unobserve(lazyImage);
+          }
+        }
+      });
+    });
 
-        lazyImages.forEach(function(lazyImage) {
-            lazyImageObserver.observe(lazyImage);
-        });
-    } else {
-        // todo fallback
-    }
+    lazyImages.forEach(function(lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  } else {
+    // todo fallback
+  }
 });
