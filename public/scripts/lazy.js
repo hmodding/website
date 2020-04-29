f372b1cddf17d2d32394f45ca294c2292c44ca52
traxam <tr808axm@gmail.com>
2020-04-29 16:39:59 +0200

    Add fallback for lazy image loading (#214)

--- public/scripts/lazy.js
+++ public/scripts/lazy.js
@@ -1,8 +1,11 @@
 /* eslint-disable no-undef */
 'use strict';
 
-/*
+/**
  * Lazy image loading.
+ * Taken from the Google Web Fundamentals, slightly modified:
+ * https://developers.google.com/
+ * web/fundamentals/performance/lazy-loading-guidance/images-and-video
  */
 document.addEventListener('DOMContentLoaded', function() {
   var lazyImages = [].slice.call(document.querySelectorAll('img.lazy-image'));
@@ -25,6 +28,38 @@ document.addEventListener('DOMContentLoaded', function() {
       lazyImageObserver.observe(lazyImage);
     });
   } else {
-    // todo fallback
+    var active = false;
+    var lazyLoad = function() {
+      if (active === false) {
+        active = true;
+
+        setTimeout(function() {
+          lazyImages.forEach(function(lazyImage) {
+            var rect = lazyImage.getBoundingClientRect();
+            if ((rect.top <= window.innerHeight && rect.bottom >= 0) &&
+                getComputedStyle(lazyImage).display !== 'none') {
+              lazyImage.src = lazyImage.dataset.src;
+              lazyImage.classList.remove('lazy-image');
+
+              lazyImages = lazyImages.filter(function(image) {
+                return image !== lazyImage;
+              });
+
+              if (lazyImages.length === 0) {
+                document.removeEventListener('scroll', lazyLoad);
+                window.removeEventListener('resize', lazyLoad);
+                window.removeEventListener('orientationchange', lazyLoad);
+              }
+            }
+          });
+
+          active = false;
+        }, 200);
+      }
+    };
+
+    document.addEventListener('scroll', lazyLoad);
+    window.addEventListener('resize', lazyLoad);
+    window.addEventListener('orientationchange', lazyLoad);
   }
 });
